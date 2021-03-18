d945c87eec58b0fc074e3f1260d6680ddd6771e4
Deep Gaurav <deepgauravraj@gmail.com>
2021-03-18 19:11:16 +0530

    even more log

--- hand.py
+++ hand.py
@@ -186,10 +186,10 @@ class Hand(object):
 
     def _draw(self, strokes, line_nums, lines, removedchars, filename, stroke_colors=None, stroke_widths=None,
               line_height=60, view_width=1000, align_center=True,biases=None, styles=None):
-        # print "Strokes "
-        # print strokes
-        # print "Line nums"
-        # print line_nums
+        print "Strokes "
+        print strokes
+        print "Line nums"
+        print line_nums
         print "Removed chars"
         print removedchars
         stroke_colors = stroke_colors or ['black'] * len(lines)
