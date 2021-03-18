7e60602a7e534e2ea899d5b02eaee2c905d56177
Deep Gaurav <deepgauravraj@gmail.com>
2021-03-18 20:32:30 +0530

    ffx

--- hand.py
+++ hand.py
@@ -254,6 +254,8 @@ class Hand(object):
                     strokes[:, 0] += (view_width - strokes[:, 0].max()) / 2
                 
                 print "segment starts at ",zip(*strokes.T)[0][0]
+                if split_num>0:
+                    strokes[:,0]-=zip(*strokes.T)[0][0]
                 strokes[:,0]+=lastshift
                 
 
